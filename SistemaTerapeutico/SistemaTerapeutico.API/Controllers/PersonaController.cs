f71246c34c0bf7b8632a22d3eb360e7d452d8c4f
nautex <jjgeorgeh@gmail.com>
2021-06-08 23:14:46 -0500

    Se retiraron using innecesarios

--- SistemaTerapeutico/SistemaTerapeutico.API/Controllers/PersonaController.cs
+++ SistemaTerapeutico/SistemaTerapeutico.API/Controllers/PersonaController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaTerapeutico.Core.Entities;
 using SistemaTerapeutico.Core.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SistemaTerapeutico.API.Controllers
--- SistemaTerapeutico/SistemaTerapeutico.API/Startup.cs
+++ SistemaTerapeutico/SistemaTerapeutico.API/Startup.cs
@@ -1,19 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SistemaTerapeutico.Core.Interfaces;
 using SistemaTerapeutico.Infrastucture.Data;
 using SistemaTerapeutico.Infrastucture.Repositorios;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SistemaTerapeutico.BackEnd.API
 {
